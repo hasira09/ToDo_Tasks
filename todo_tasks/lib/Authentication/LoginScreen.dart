c57050973cf76963be467623eaf242550b7f0036
hasira09 <hasiramahel09@gmail.com>
2023-05-20 01:07:26 +0530

    Flutter Auth UI - III

--- todo_tasks/lib/Authentication/LoginScreen.dart
+++ todo_tasks/lib/Authentication/LoginScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'RegistrationScreen.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -25,51 +27,117 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                hintText: "Enter Email",
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-              onChanged: (value) {
-                _email = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                hintText: "Enter Password",
+              const Image(
+                image: AssetImage("assets/Images/Logo.png"),
+                height: 150,
+                width: 220,
               ),
-              onChanged: (value) {
-                _password = value;
-              },
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: SizedBox(
-                width: 350,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      )),
-                  child: const Text("Log - In",
-                      style: TextStyle(fontSize: 18)),
-                  onPressed: () => loginPressed(),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                  prefixIcon: const Icon(Icons.person,
+                      color: Color.fromRGBO(184, 43, 44, 1)),
+                ),
+                onChanged: (value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.lock_outline_rounded,
+                      color: Color.fromRGBO(184, 43, 44, 1),
+                    )),
+                onChanged: (value) {
+                  _password = value;
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )),
+                    child:
+                        const Text("Log - In", style: TextStyle(fontSize: 18)),
+                    onPressed: () => loginPressed(),
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 40),
+              const Image(
+                  image: AssetImage("assets/Buttons/Or.png"),
+                  height: 50,
+                  width: 300),
+              GestureDetector(
+                onTap: () {},
+                child: const Image(
+                    image: AssetImage("assets/Buttons/GoogleButton.png"),
+                    height: 100,
+                    width: 250),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't Have an Account? ",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const RegistrationScreen()));
+                    },
+                    child: const Text(
+                      "Sign-In",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
