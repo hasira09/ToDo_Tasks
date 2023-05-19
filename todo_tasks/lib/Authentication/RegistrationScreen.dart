5a0b58da732e94df17f6f88de1f17f409b96d3fc
hasira09 <hasiramahel09@gmail.com>
2023-05-19 18:22:44 +0530

    Flutter Auth - II

--- todo_tasks/lib/Authentication/RegistrationScreen.dart
+++ todo_tasks/lib/Authentication/RegistrationScreen.dart
@@ -17,120 +17,145 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        centerTitle: true,
-        elevation: 0,
-        title: const Text(
-          "Registration",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            const Image(
-              image: AssetImage("assets/Images/Logo.png"),
-              height: 100,
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Enter Name",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.black),
-                ),
-                prefixIcon: const Icon(Icons.person),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
               ),
-              onChanged: (value) {
-                _name = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Enter Email",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.black),
-                ),
-                prefixIcon: const Icon(Icons.email_rounded),
+              const Image(
+                image: AssetImage("assets/Images/Logo.png"),
+                height: 150,
+                width: 220,
               ),
-              onChanged: (value) {
-                _email = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Enter Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.black),
-                ),
-                prefixIcon: const Icon(Icons.lock_outline_rounded),
+              const SizedBox(
+                height: 30,
               ),
-              onChanged: (value) {
-                _password = value;
-              },
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: SizedBox(
-                width: 350,
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                  prefixIcon: const Icon(Icons.person,
+                      color: Color.fromRGBO(184, 43, 44, 1)),
+                ),
+                onChanged: (value) {
+                  _name = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                  prefixIcon: const Icon(Icons.email_rounded,
+                      color: Color.fromRGBO(184, 43, 44, 1)),
+                ),
+                onChanged: (value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                  prefixIcon: const Icon(Icons.lock_outline_rounded,
+                      color: Color.fromRGBO(184, 43, 44, 1)),
+                ),
+                onChanged: (value) {
+                  _password = value;
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: SizedBox(
+                  width: 320,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )),
+                    child: const Text("Create Account",
+                        style: TextStyle(fontSize: 18)),
+                    onPressed: () => createAccountPressed(),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Image(
+                image: AssetImage("assets/Buttons/Or.png"),
                 height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      )),
-                  child: const Text("Create Account",
-                      style: TextStyle(fontSize: 18)),
-                  onPressed: () => createAccountPressed(),
+                width: 300,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: const Image(
+                  image: AssetImage("assets/Buttons/GoogleButton.png"),
+                  height: 100,
+                  width: 250,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const LoginScreen()));
-              },
-              child: const Text(
-                "Already Have an Account?",
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
-                ),
+              const SizedBox(
+                height: 30,
               ),
-            )
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already Have an Account? ",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    },
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
