29c0cbc61b1de3c7ec24a714a353468129ced943
hasira09 <hasiramahel09@gmail.com>
2023-05-21 12:59:11 +0530

    Flutter Integration
    A Server needed to be configured for a fully functional authentication. That will be the next step.

--- todo_tasks/lib/Services/Globals.dart
+++ todo_tasks/lib/Services/Globals.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const String baseURL = "http://10.0.2.2:8000/ap/"; //Emulator Localhost
+const String baseURL = "http://10.0.2.2:8000/api/"; //Emulator Localhost
 const Map<String, String> headers = {"Content-Type": "application/json"};
 
 errorSnackBar(BuildContext context, String text) {
